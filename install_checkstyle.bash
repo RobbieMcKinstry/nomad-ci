b55f4ca022bf81fb522ed539ec370feec239f008
Robbie McKinstry <robbie@hashicorp.com>
2017-07-15 21:19:14 -0400

    whoops, need the main.go file for `go get` to work

--- install_checkstyle.bash
+++ install_checkstyle.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo 'destination is '$DESTINATION
 readonly ARCHIVE='checkstyle.tar.gz'
-readonly FINAL_DEST=$DESTNATION/checkstyle
 
 mkdir -p $FINAL_DEST || 0
 
@@ -9,6 +7,9 @@ cd $FINAL_DEST
 
 curl -s -L 'https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.0/checkstyle-8.0-all.jar?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcheckstyle%2Ffiles%2Fcheckstyle%2F&ts=1499992368&use_mirror=superb-sea2' >> $ARCHIVE
 tar -xzf $ARCHIVE
+
+mv checkstyle $DESTINATION
 rm $ARCHIVE
+
 
 cd -
--- install_findbugs.bash
+++ install_findbugs.bash
@@ -5,4 +5,7 @@ readonly FINDBUGS_VERSION='findbugs-3.0.1'
 curl -s -L 'https://downloads.sourceforge.net/project/findbugs/findbugs/3.0.1/findbugs-3.0.1.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Ffindbugs%2Ffiles%2Ffindbugs%2F3.0.1%2Ffindbugs-3.0.1.tar.gz%2Fdownload%3Fuse_mirror%3Dcfhcable%26download%3D&ts=1499989850&use_mirror=managedway' >> $ARCHIVE
 tar -xzf $ARCHIVE
 
+rm $ARCHIVE
+rm -r $FINDBUGS_VERSION
+
 mv $FINDBUGS_VERSION/lib/findbugs.jar $DESTINATION/findbugs.jar
