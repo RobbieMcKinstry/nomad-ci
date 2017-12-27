fa01335b1a6533b92f79eebbb64278b1648b4da7
Robbie McKinstry <robbie@hashicorp.com>
2017-12-27 02:06:22 -0500

    finally works if you can believe it

--- install_checkstyle.bash
+++ install_checkstyle.bash
@@ -1,15 +1,13 @@
 #!/bin/bash
+
+set -e
+set -x
+set -u
 readonly ARCHIVE='checkstyle.tar.gz'
+readonly DESTINATION='/opt'
 
-mkdir -p $FINAL_DEST || 0
+mkdir -p $DESTINATION || 0
 
-cd $FINAL_DEST
+curl -s -L 'https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.0/checkstyle-8.0-all.jar?r=&ts=1514357386&use_mirror=versaweb' > $ARCHIVE
 
-curl -s -L 'https://downloads.sourceforge.net/project/checkstyle/checkstyle/8.0/checkstyle-8.0-all.jar?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcheckstyle%2Ffiles%2Fcheckstyle%2F&ts=1499992368&use_mirror=superb-sea2' >> $ARCHIVE
-tar -xzf $ARCHIVE
-
-mv checkstyle $DESTINATION
-rm $ARCHIVE
-
-
-cd -
+mv $ARCHIVE $DESTINATION/checkstyle.jar
--- install_findbugs.bash
+++ install_findbugs.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
+set -e
+set -x
+set -u
 readonly ARCHIVE='findbugs.tar.gz'
 readonly FINDBUGS_VERSION='findbugs-3.0.1'
+readonly DESTINATION='/opt'
 
 curl -s -L 'https://downloads.sourceforge.net/project/findbugs/findbugs/3.0.1/findbugs-3.0.1.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Ffindbugs%2Ffiles%2Ffindbugs%2F3.0.1%2Ffindbugs-3.0.1.tar.gz%2Fdownload%3Fuse_mirror%3Dcfhcable%26download%3D&ts=1499989850&use_mirror=managedway' >> $ARCHIVE
 tar -xzf $ARCHIVE
 
 rm $ARCHIVE
-rm -r $FINDBUGS_VERSION
-
+mkdir -p $DESTINATION
 mv $FINDBUGS_VERSION/lib/findbugs.jar $DESTINATION/findbugs.jar
+
+rm -r $FINDBUGS_VERSION
